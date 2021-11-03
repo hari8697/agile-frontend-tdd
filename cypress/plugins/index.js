203dda614a53224568db969b033d10aa0b5ed6fa
hari8697 <hari8697@gmail.com>
2021-11-03 06:53:19 +0530

    chore: Specify app should list restaurants
    Added .env vars for api key in cypress config and app.js
    
    Added dotenv package

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,16 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+
+require("dotenv").config()
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+
+  // copy any needed variables from process.env to config.env
+  config.env.api_key = process.env.REACT_APP_API_KEY
+
+  // do not forget to return the changed config object!
+  return config
 }
--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
-const App = () => <div>Hello, world.</div>
+// import axios from "axios"
+
+const App = () => {
+  // console.log(process.env.REACT_APP_API_KEY)
+
+  // const handleClick = () => {
+  //   axios
+  //     .post(
+  //       `https://outside-in-dev-api.herokuapp.com/${process.env.REACT_APP_API_KEY}/restaurants`,
+  //       {name: "Sushi Place"},
+  //     )
+  //     .then(response => console.log(response.data))
+  // }
+
+  return <div>Hello, world.</div>
+}
 
 export default App
