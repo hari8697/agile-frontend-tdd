1ecfdd9506ffeba3fc5efc9147d1f346a66fa314
hari8697 <hari8697@gmail.com>
2021-11-06 07:38:37 +0530

    test: When save succeeds

--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -118,6 +118,8 @@ describe("restaurants", () => {
 
   describe("createRestaurant action", () => {
     const newRestaurantName = "Sushi Place"
+    const existingRestaurant = { id: 1, name: "Pizza Place" }
+    const responseRestaurant = { id: 2, name: newRestaurantName }
 
     let api
     let store
@@ -127,7 +129,7 @@ describe("restaurants", () => {
         createRestaurant: jest.fn().mockName("createRestaurant"),
       }
 
-      const initialState = {}
+      const initialState = { records: [existingRestaurant] }
 
       store = createStore(
         restaurantsReducer,
@@ -137,8 +139,23 @@ describe("restaurants", () => {
     })
 
     it("saves the restaurant to the server", () => {
+      api.createRestaurant.mockResolvedValue(responseRestaurant)
       store.dispatch(createRestaurant(newRestaurantName))
       expect(api.createRestaurant).toHaveBeenCalledWith(newRestaurantName)
+    })
+
+    describe("when save succeeds", () => {
+      beforeEach(() => {
+        api.createRestaurant.mockResolvedValue(responseRestaurant)
+        store.dispatch(createRestaurant(newRestaurantName))
+      })
+
+      it("stores the returned restaurant in the store", () => {
+        expect(store.getState().records).toEqual([
+          existingRestaurant,
+          responseRestaurant,
+        ])
+      })
     })
   })
 })
